3bf620d64536a8bf70d0d61f1ec4c10d4874de9f
ravi99 <menariyaravi6@gmail.com>
2024-09-19 10:52:31 +0530

    updating after setup

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,25 @@
-const express = require('express') ;
+const express = require('express');
 const colors = require('colors')
-const app = express() ;
-const PORT = 3000 ;
+const app = express();
+const PORT = 3000;
 const dbConnect = require('./db')
-const errorHandler  = require('./middleware/errorHandler')
+const errorHandler = require('./middleware/errorHandler')
 //parse the data from the req.body
 app.use(express.json());
 
 dbConnect()
 
 
-app.use('/api',require('./routes/userRoutes'))
+app.use('/api', require('./routes/userRoutes'))
 
 //global error handler middleware
-app.use(errorHandler) ;
+app.use(errorHandler);
 
-app.listen(PORT , ()=>{
+app.get('/register', (req, res) => {
+    console.log("on register");
+    res.send("on register route");
+});
+
+app.listen(PORT, () => {
     console.log(colors.yellow(`App is listening on the PORT:${PORT}`))
 })
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,14 @@
 //express import 
-const express = require('express') ;
+const express = require('express');
 const userController = require('../controllers/userController')
 //express.Router()
 const router = express.Router();
 
-router.post('/register',userController.signUp) ;
-router.post('/login',userController.login) ;
+router.post('/register', userController.signUp);
+router.post('/login', userController.login);
+router.get('/register', (req, res) => {
+    res.send(" on register in api");
+}
+);
 
-module.exports = router ;
+module.exports = router;
